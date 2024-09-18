cd6c03090f857c7857b932fe45035b9407d2d19d
Flávia Figueredo <flaviagfigueredo@gmail.com>
2024-09-18 20:18:23 -0300

    feat: add Footer component

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,14 @@
+import { Layout } from "antd" // importa o Layout do Ant Design
+import "./styles.css" // importa o arquivo de estilos
+
+// renomeia o subcomponente Footer do Layout como AntFooter para evitar conflitos com o componente Footer da aplicação
+const { Footer: AntFooter } = Layout
+
+// função que define o componente Footer
+export function Footer() {
+    return (
+        <AntFooter className="footer">
+            <p>© 2024 RickAndMortyHub - Todos os direitos reservados</p>
+        </AntFooter>
+    )
+}
