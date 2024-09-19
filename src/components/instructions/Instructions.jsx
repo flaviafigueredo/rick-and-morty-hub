be0672d8409b21d7fc17d457e86c8798753c6ed3
Flávia Figueredo <flaviagfigueredo@gmail.com>
2024-09-19 14:45:16 -0300

    feat: add Instructions component

---
+++ src/components/instructions/Instructions.jsx
@@ -0,0 +1,8 @@
+// componente para exibir instruções para o usuário
+export function Instructions() {
+    return (
+        <div>
+            <p>Search for a Rick and Morty character by typing their name. Their details will be fetched from the <a href="https://rickandmortyapi.com/" target="_blank">Rick and Morty API</a>.</p>
+        </div>
+    )
+}
